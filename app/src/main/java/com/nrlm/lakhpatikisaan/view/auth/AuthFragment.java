380f86cc6d5ec96f7c893593dbcd447ba3c0f710
Abdussamad <abdussamads789@gmail.com>
2021-12-06 14:29:46 +0530

    Server dialoge & login valdsn done

--- app/src/main/java/com/nrlm/lakhpatikisaan/view/auth/AuthFragment.java
+++ app/src/main/java/com/nrlm/lakhpatikisaan/view/auth/AuthFragment.java
@@ -1,6 +1,7 @@
 package com.nrlm.lakhpatikisaan.view.auth;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -12,6 +13,8 @@ import androidx.annotation.Nullable;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.NavDirections;
 
+import com.google.android.material.textfield.TextInputEditText;
+import com.nrlm.lakhpatikisaan.R;
 import com.nrlm.lakhpatikisaan.databinding.FragmentAuthLoginBinding;
 import com.nrlm.lakhpatikisaan.network.client.Result;
 import com.nrlm.lakhpatikisaan.network.model.request.LogRequestBean;
@@ -20,8 +23,11 @@ import com.nrlm.lakhpatikisaan.repository.MasterDataRepo;
 import com.nrlm.lakhpatikisaan.repository.RepositoryCallback;
 import com.nrlm.lakhpatikisaan.utils.AppExecutor;
 import com.nrlm.lakhpatikisaan.utils.AppUtils;
+import com.nrlm.lakhpatikisaan.utils.DialogFactory;
 import com.nrlm.lakhpatikisaan.view.BaseFragment;
 import com.nrlm.lakhpatikisaan.view.home.HomeActivity;
+
+import org.json.JSONException;
 
 public class AuthFragment extends BaseFragment<AuthViewModel, FragmentAuthLoginBinding> {
 
@@ -49,11 +55,7 @@ public class AuthFragment extends BaseFragment<AuthViewModel, FragmentAuthLoginB
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        AuthViewModel authViewModel=   new ViewModelProvider(this).get(AuthViewModel.class);
-        authViewModel.makeLoginRequestData(getContext());
-
-
-
+        AuthViewModel authViewModel = new ViewModelProvider(this).get(AuthViewModel.class);
 
         binding.tvForgetPassword.setOnClickListener(v -> {
             NavDirections action = AuthFragmentDirections.actionAuthFragmentToSendOtpFragment();
@@ -61,13 +63,62 @@ public class AuthFragment extends BaseFragment<AuthViewModel, FragmentAuthLoginB
         });
 
         binding.btnLogin.setOnClickListener(v -> {
+            String password = binding.etPassword.getText().toString();
+            String userId = binding.etUserId.getText().toString();
+            if (userId.equalsIgnoreCase("")){
+                binding.etUserId.setError("Invalid user id");
+            }else if(password.equalsIgnoreCase("")) {
+                binding.etPassword.setError("Invalid password");
+            }else {
+                String loginApiStatus=authViewModel.makeLoginRequestData(getContext());
+                if (!loginApiStatus.equalsIgnoreCase("")){
+                    Intent intent = new Intent(getContext(), HomeActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(intent);
+                }else {
+                    try {
+                        showServerError(loginApiStatus);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
 
-            Intent intent = new Intent(getContext(), HomeActivity.class);
-            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-            startActivity(intent);
         });
     }
+    public  void showServerError(String error_code) throws JSONException {
+        switch (error_code) {
 
+            case "E202":
+                DialogFactory.getInstance().showAlertDialog(getCurrentContext(), 1, getString(R.string.info), getString(R.string.invalid_fields)
+                        , getString(R.string.ok), (DialogInterface.OnClickListener) (dialog, which) -> dialog.dismiss(), null, null, false
+                );
 
+                break;
+
+            case "E201":
+                DialogFactory.getInstance().showAlertDialog(getCurrentContext(), 1, getString(R.string.info), getString(R.string.error_security_validation)
+                        , getString(R.string.ok), (dialog, which) -> dialog.dismiss(), null, null, false
+                );
+                break;
+
+            case "E1004":
+                DialogFactory.getInstance().showAlertDialog(getCurrentContext(), 1, getString(R.string.info), getString(R.string.error_validation)
+                        , getString(R.string.ok), (dialog, which) -> dialog.dismiss(), null, null, false
+                );
+                break;
+
+            case "E206":
+                DialogFactory.getInstance().showAlertDialog(getCurrentContext(), 1, getString(R.string.info), getString(R.string.error_session_exist)
+                        , getString(R.string.ok), (dialog, which) -> dialog.dismiss(), null, null, false
+                );
+                break;
+
+            default:
+                DialogFactory.getInstance().showAlertDialog(getCurrentContext(), 1, getString(R.string.info), getString(R.string.SERVER_ERROR_TITLE)
+                        , getString(R.string.ok), (dialog, which) -> dialog.dismiss(), null, null, false
+                );
+        }
+    }
 
 }
--- app/src/main/java/com/nrlm/lakhpatikisaan/view/auth/AuthViewModel.java
+++ app/src/main/java/com/nrlm/lakhpatikisaan/view/auth/AuthViewModel.java
@@ -21,120 +21,125 @@ import com.nrlm.lakhpatikisaan.view.home.HomeViewModel;
 public class AuthViewModel extends ViewModel {
     private LoginRepo loginRepo;
     private MasterDataRepo masterDataRepo;
-    public AuthViewModel (){
+    private String loginApiStatus="";
+
+    public AuthViewModel() {
 
     }
 
-   public void  makeLoginRequestData(Context context){
-       loginRepo=LoginRepo.getInstance(AppExecutor.getInstance().threadExecutor(),context);
-       masterDataRepo=MasterDataRepo.getInstance(AppExecutor.getInstance().threadExecutor(),context);
-      final LoginRequestBean  loginRequestBean=new LoginRequestBean();
-       loginRequestBean.setAndroid_api_version("1.0.0");
-       loginRequestBean.setAndroid_version("1.0.0");
-       loginRequestBean.setApp_login_time("2021-04-13 16:33:23");
-       loginRequestBean.setApp_versions("1.0.0");
-       loginRequestBean.setDate("06-12-2021");
-       loginRequestBean.setDevice_name("OPPO-OP4B79L1-CPH1933");
-       loginRequestBean.setImei_no("5d7eaa5ef9d3ebed");
-       loginRequestBean.setLocation_coordinate("28.6771787,77.4923927");
-       loginRequestBean.setLogout_time("2021-04-13 16:33:23");
-       loginRequestBean.setLogin_id("HRKSVISHAKHA");
-       loginRequestBean.setPassword("c6024fd19953c32dc6e2b8fe91684a16a889cc8482157f1ec652616517537239");
-       makeLogin(loginRequestBean);
-
-   }
-
-    public void makeLogin(LoginRequestBean loginRequestBean){
-       loginRepo.makeLoginRequest(loginRequestBean, new RepositoryCallback() {
-           @Override
-           public void onComplete(Result result) {
-               try {
-               AppUtils.getInstance().showLog("loginDataResult"+result.toString(), AuthViewModel.class);
-               if (result instanceof Result.Success){
-                   LoginResponseBean loginResponseBean= (LoginResponseBean) ((Result.Success) result).data;
-                   AppUtils.getInstance().showLog("loginDataResponseBean"+loginResponseBean.getError().getCode()+"---"+loginResponseBean.getError().getMessage(), AuthViewModel.class);
-
-                   if (loginResponseBean.getError().getCode().equalsIgnoreCase("E200")){
-                          LogRequestBean logRequestBean=new LogRequestBean("UPAGASSDAD","up"
-                                   ,"111","111","111");
-
-                           masterDataRepo.makeMasterDataRequest(logRequestBean, new RepositoryCallback() {
-                               @Override
-                               public void onComplete(Result result) {
-                                   AppUtils.getInstance().showLog("masterDataResult"+result.toString(),AuthViewModel.class);
-                                   if (result instanceof Result.Success){
-                                       MasterDataResponseBean masterDataResponseBean= (MasterDataResponseBean) ((Result.Success) result).data;
-                                       AppUtils.getInstance().showLog("masterDataResponseBean"+masterDataResponseBean.getError().getCode()+"---"
-                                               +masterDataResponseBean.getError().getMessage(),AuthViewModel.class);
-
-                                   }else {
-                                       Object errorObject=((Result.Error) result).exception ;
-                                       if (errorObject != null){
-                                           if (errorObject instanceof MasterDataResponseBean.Error){
-                                               MasterDataResponseBean.Error responseError= (MasterDataResponseBean.Error) errorObject;
-                                               AppUtils.getInstance().showLog(responseError.getCode()+" MasterApiErrorObj"
-                                                       +responseError.getMessage(),AuthViewModel.class);
-                                           } else  if (errorObject instanceof Throwable){
-                                               Throwable exception= (Throwable)errorObject;
-                                               AppUtils.getInstance().showLog("MasterRetrofitErrors:-------"+exception.getMessage()
-                                                       ,AuthViewModel.class);
-                                           }
-                                       }
-
-                                   }
-                               }
-                           });
-
-                           masterDataRepo.makeSupportiveMasterDataRequest(logRequestBean, new RepositoryCallback() {
-                               @Override
-                               public void onComplete(Result result) {
-                                   AppUtils.getInstance().showLog("supportiveMasterDataResult"+result.toString(),AuthViewModel.class);
-                                   if (result instanceof Result.Success){
-                                       SupportiveMastersResponseBean supportiveMastersResponseBean = (SupportiveMastersResponseBean) ((Result.Success) result).data;
-                                       AppUtils.getInstance().showLog("supportiveMasterDataResponseBean"+supportiveMastersResponseBean.getError().getCode()+"---"
-                                               +supportiveMastersResponseBean.getError().getMessage(),AuthViewModel.class);
-
-                                   }else {
-                                       Object errorObject=((Result.Error) result).exception ;
-                                       if (errorObject != null){
-                                           if (errorObject instanceof SupportiveMastersResponseBean.Error){
-                                               SupportiveMastersResponseBean.Error responseError= (SupportiveMastersResponseBean.Error) errorObject;
-                                               AppUtils.getInstance().showLog(responseError.getCode()+"SupportiveMasterApiErrorObj"
-                                                       +responseError.getMessage(),AuthViewModel.class);
-                                           } else  if (errorObject instanceof Throwable){
-                                               Throwable exception= (Throwable)errorObject;
-                                               AppUtils.getInstance().showLog("SupportiveMasterRetrofitErrors:-------"+exception.getMessage()
-                                                       ,AuthViewModel.class);
-                                           }
-                                       }
-
-                                   }
-
-                               }
-                           });
-                   }
-
-               }else {
-                   Object errorObject=((Result.Error) result).exception ;
-                   if (errorObject != null){
-                       if (errorObject instanceof MasterDataResponseBean.Error){
-                           LoginResponseBean.Error responseError= (LoginResponseBean.Error) errorObject;
-                           AppUtils.getInstance().showLog(responseError.getCode()+" loginApiErrorObj"+responseError.getMessage(),AuthViewModel.class);
-                       } else  if (errorObject instanceof Throwable){
-                           Throwable exception= (Throwable)errorObject;
-                           AppUtils.getInstance().showLog("RetrofitErrorsLogin:-------"+exception.getMessage(),AuthViewModel.class);
-                       }
-                   }
-
-               }
-               }catch (Exception e){
-                   AppUtils.getInstance().showLog("Exceptioncall"+e,DashBoardFragment.class);
-               }
-           }
-       });
+    public String makeLoginRequestData(Context context) {
+        loginRepo = LoginRepo.getInstance(AppExecutor.getInstance().threadExecutor(), context);
+        masterDataRepo = MasterDataRepo.getInstance(AppExecutor.getInstance().threadExecutor(), context);
+        final LoginRequestBean loginRequestBean = new LoginRequestBean();
+        loginRequestBean.setAndroid_api_version("1.0.0");
+        loginRequestBean.setAndroid_version("1.0.0");
+        loginRequestBean.setApp_login_time("2021-04-13 16:33:23");
+        loginRequestBean.setApp_versions("1.0.0");
+        loginRequestBean.setDate("06-12-2021");
+        loginRequestBean.setDevice_name("OPPO-OP4B79L1-CPH1933");
+        loginRequestBean.setImei_no("5d7eaa5ef9d3ebed");
+        loginRequestBean.setLocation_coordinate("28.6771787,77.4923927");
+        loginRequestBean.setLogout_time("2021-04-13 16:33:23");
+        loginRequestBean.setLogin_id("HRKSVISHAKHA");
+        loginRequestBean.setPassword("c6024fd19953c32dc6e2b8fe91684a16a889cc8482157f1ec652616517537239");
+        makeLogin(loginRequestBean);
+        return loginApiStatus;
     }
 
-    public String loginApiResult(String result){
+    public void makeLogin(LoginRequestBean loginRequestBean) {
+
+        loginRepo.makeLoginRequest(loginRequestBean, new RepositoryCallback() {
+            @Override
+            public void onComplete(Result result) {
+                try {
+                    AppUtils.getInstance().showLog("loginDataResult" + result.toString(), AuthViewModel.class);
+                    if (result instanceof Result.Success) {
+                        LoginResponseBean loginResponseBean = (LoginResponseBean) ((Result.Success) result).data;
+                        AppUtils.getInstance().showLog("loginDataResponseBean" + loginResponseBean.getError().getCode() + "---" + loginResponseBean.getError().getMessage(), AuthViewModel.class);
+                        loginApiStatus = "E200";
+                        if (loginResponseBean.getError().getCode().equalsIgnoreCase("E200")) {
+                            LogRequestBean logRequestBean = new LogRequestBean("UPAGASSDAD", "up"
+                                    , "111", "111", "111");
+
+                            masterDataRepo.makeMasterDataRequest(logRequestBean, new RepositoryCallback() {
+                                @Override
+                                public void onComplete(Result result) {
+                                    AppUtils.getInstance().showLog("masterDataResult" + result.toString(), AuthViewModel.class);
+                                    if (result instanceof Result.Success) {
+                                        MasterDataResponseBean masterDataResponseBean = (MasterDataResponseBean) ((Result.Success) result).data;
+                                        AppUtils.getInstance().showLog("masterDataResponseBean" + masterDataResponseBean.getError().getCode() + "---"
+                                                + masterDataResponseBean.getError().getMessage(), AuthViewModel.class);
+
+                                    } else {
+                                        Object errorObject = ((Result.Error) result).exception;
+                                        if (errorObject != null) {
+                                            if (errorObject instanceof MasterDataResponseBean.Error) {
+                                                MasterDataResponseBean.Error responseError = (MasterDataResponseBean.Error) errorObject;
+                                                AppUtils.getInstance().showLog(responseError.getCode() + " MasterApiErrorObj"
+                                                        + responseError.getMessage(), AuthViewModel.class);
+                                            } else if (errorObject instanceof Throwable) {
+                                                Throwable exception = (Throwable) errorObject;
+                                                AppUtils.getInstance().showLog("MasterRetrofitErrors:-------" + exception.getMessage()
+                                                        , AuthViewModel.class);
+                                            }
+                                        }
+
+                                    }
+                                }
+                            });
+
+                            masterDataRepo.makeSupportiveMasterDataRequest(logRequestBean, new RepositoryCallback() {
+                                @Override
+                                public void onComplete(Result result) {
+                                    AppUtils.getInstance().showLog("supportiveMasterDataResult" + result.toString(), AuthViewModel.class);
+                                    if (result instanceof Result.Success) {
+                                        SupportiveMastersResponseBean supportiveMastersResponseBean = (SupportiveMastersResponseBean) ((Result.Success) result).data;
+                                        AppUtils.getInstance().showLog("supportiveMasterDataResponseBean" + supportiveMastersResponseBean.getError().getCode() + "---"
+                                                + supportiveMastersResponseBean.getError().getMessage(), AuthViewModel.class);
+
+                                    } else {
+                                        Object errorObject = ((Result.Error) result).exception;
+                                        if (errorObject != null) {
+                                            if (errorObject instanceof SupportiveMastersResponseBean.Error) {
+                                                SupportiveMastersResponseBean.Error responseError = (SupportiveMastersResponseBean.Error) errorObject;
+                                                AppUtils.getInstance().showLog(responseError.getCode() + "SupportiveMasterApiErrorObj"
+                                                        + responseError.getMessage(), AuthViewModel.class);
+                                            } else if (errorObject instanceof Throwable) {
+                                                Throwable exception = (Throwable) errorObject;
+                                                AppUtils.getInstance().showLog("SupportiveMasterRetrofitErrors:-------" + exception.getMessage()
+                                                        , AuthViewModel.class);
+                                            }
+                                        }
+
+                                    }
+
+                                }
+                            });
+                        }
+
+                    } else {
+                        Object errorObject = ((Result.Error) result).exception;
+                        if (errorObject != null) {
+                            if (errorObject instanceof MasterDataResponseBean.Error) {
+                                LoginResponseBean.Error responseError = (LoginResponseBean.Error) errorObject;
+                                loginApiStatus=responseError.getCode();
+                                AppUtils.getInstance().showLog(responseError.getCode() + " loginApiErrorObj" + responseError.getMessage(), AuthViewModel.class);
+                            } else if (errorObject instanceof Throwable) {
+                                Throwable exception = (Throwable) errorObject;
+                                loginApiStatus=exception.getMessage();
+                                AppUtils.getInstance().showLog("RetrofitErrorsLogin:-------" + exception.getMessage(), AuthViewModel.class);
+                            }
+                        }
+
+                    }
+                } catch (Exception e) {
+                    AppUtils.getInstance().showLog("Exceptioncall" + e, DashBoardFragment.class);
+                }
+            }
+        });
+    }
+
+    public String loginApiResult(String result) {
         return result;
     }
 }
