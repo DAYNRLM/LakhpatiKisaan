2a19c676dfd0154e31957060f06f13864fbb3379
linconbhalla <linconbhalla007@gmail.com>
2021-12-16 18:57:34 +0530

    logout dialog error is  done

--- app/src/main/java/com/nrlm/lakhpatikisaan/view/dialogFragment/LogOutDialogFragment.java
+++ app/src/main/java/com/nrlm/lakhpatikisaan/view/dialogFragment/LogOutDialogFragment.java
@@ -11,37 +11,38 @@ import androidx.fragment.app.DialogFragment;
 
 import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import com.nrlm.lakhpatikisaan.R;
+import com.nrlm.lakhpatikisaan.repository.LoginRepo;
 import com.nrlm.lakhpatikisaan.utils.AppDateFactory;
+import com.nrlm.lakhpatikisaan.utils.AppExecutor;
 import com.nrlm.lakhpatikisaan.utils.AppUtils;
 import com.nrlm.lakhpatikisaan.utils.CustomProgressDialog;
+import com.nrlm.lakhpatikisaan.utils.PreferenceFactory;
+import com.nrlm.lakhpatikisaan.utils.PreferenceKeyManager;
 import com.nrlm.lakhpatikisaan.utils.ViewUtilsKt;
 import com.nrlm.lakhpatikisaan.view.auth.AuthActivity;
 
 public class LogOutDialogFragment extends DialogFragment {
     AlertDialog alertDialog;
     CustomProgressDialog customProgressDialog;
-    //HomeRepository homeRepository;
+    LoginRepo loginRepo;
     AppUtils appUtils;
-   // AppSharedPreferences appSharedPreferences;
     AppDateFactory appDateFactory;
 
     @NonNull
     @Override
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
         customProgressDialog =CustomProgressDialog.newInstance(requireContext());
-       // homeRepository = HomeRepository.getInstance(getActivity().getApplication());
+        loginRepo = LoginRepo.getInstance(AppExecutor.getInstance().threadExecutor(), requireContext());
         appUtils=AppUtils.getInstance();
         appDateFactory= AppDateFactory.getInstance();
-       // appSharedPreferences=AppSharedPreferences.getInstance(requireContext());
         alertDialog =   new MaterialAlertDialogBuilder(requireContext()).setIcon(R.drawable.ic_baseline_logout_24)
                 .setTitle(getContext().getResources().getString(R.string.dialog_sign_out_title)).setMessage(getContext().getResources().getString(R.string.dialog_sign_out_msg))
                 .setCancelable(false)
                 .setPositiveButton(getContext().getResources().getString(R.string.dialog_btn_signout), (dialogInterface, i) -> {
                     dialogInterface.dismiss();
                     ViewUtilsKt.toast(requireContext(),getContext().getResources().getString(R.string.toast_success_sign_out));
-                   // homeRepository.deleteTables();
-                   // appSharedPreferences.removeDataAtLogout();
-                   // appSharedPreferences.setLogOutTime(appDateFactory.getDateTime());
+                    loginRepo.deleteAllMaster();
+                    PreferenceFactory.getInstance().saveSharedPrefrecesData(PreferenceKeyManager.getPrefLoginSessionKey(),"",requireContext());
                     Intent intent = new Intent(getContext(), AuthActivity.class);
                     intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
                     getContext().startActivity(intent);
--- app/src/main/java/com/nrlm/lakhpatikisaan/view/home/HomeActivity.java
+++ app/src/main/java/com/nrlm/lakhpatikisaan/view/home/HomeActivity.java
@@ -2,27 +2,58 @@ package com.nrlm.lakhpatikisaan.view.home;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.NavDirections;
 import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
+import com.google.android.material.navigation.NavigationView;
 import com.nrlm.lakhpatikisaan.R;
+import com.nrlm.lakhpatikisaan.utils.AppUtils;
 
 public class HomeActivity extends AppCompatActivity {
+    NavigationView navigation_view;
+    DrawerLayout home_drawer_layout;
     NavController navController;
+    AppBarConfiguration appBarConfiguration;
+    Toolbar tollBar;
+    MenuItem menuItem;
+    TextView badgeTv;
+    int notification = 0;
+    AppUtils appUtils;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+
+        tollBar = findViewById(R.id.tollBar);
+        navigation_view = findViewById(R.id.navigation_view);
+        home_drawer_layout = findViewById(R.id.home_drawer_layout);
+        appUtils = AppUtils.getInstance();
+
+        setSupportActionBar(tollBar);
+
         NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.home_nav_host);
         navController = navHostFragment.getNavController();
+
+        appBarConfiguration =
+                new AppBarConfiguration.Builder(navController.getGraph()).setOpenableLayout(home_drawer_layout).build();
+
+        NavigationUI.setupActionBarWithNavController(HomeActivity.this, navController, appBarConfiguration);
+
+        NavigationUI.setupWithNavController(navigation_view, navController);
+
+
     }
 
 
@@ -37,9 +68,8 @@ public class HomeActivity extends AppCompatActivity {
         boolean status = false;
 
         switch (item.getItemId()) {
-            case R.id.logOutDialogFragment:
-                navController.navigate(R.id.logoutDialog);
-                status = true;
+            case R.id.logOut:
+                navController.navigate(R.id.logOutDialogFragment);
                 break;
         }
         return NavigationUI.onNavDestinationSelected(item, navController) || super.onOptionsItemSelected(item);
